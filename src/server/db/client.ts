343205889578819b4c057c84b71f02585fe8f7ed
Diwan <pbldbg@gmail.com>
2025-12-10 21:21:27 +0100

    chore: switch Prisma 7 client to generated output

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,5 +1,5 @@
 // src/server/db/client.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
